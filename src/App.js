e753100a513e84c5df9fb51335c13e0c1a31ffee
Gyyk <nazik.osmak@yahoo.com>
2019-08-01 21:11:44 +0300

    Gh-pages

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
-            <h1 className="App-title"><Link to="/products" className="App-title">My simple shop</Link></h1>
+            <h1 className="App-title"><Link to="/TM-Redux-task/products" className="App-title">My simple shop</Link></h1>
             <div className='App-header-cart-section'>
-              <Link to="/cart">
+              <Link to="/TM-Redux-task/cart">
                 <img className='cart-image' src='https://icon2.kisspng.com/20180320/dvq/kisspng-shopping-cart-computer-icons-white-cart-png-simple-5ab15d02e7c8b8.7488579515215731229494.jpg' alt='Cart' />
               </Link>
               <div className='cart-count'>{this.props.inCart.length}</div>
@@ -27,8 +27,8 @@ class App extends Component {
           </header>
           <div className="App-wrapper">
             <SideBar />
-            <Route path="/products" component={ProductList} />
-            <Route path="/cart" component={Cart} />
+            <Route path="/TM-Redux-task/products" component={ProductList} />
+            <Route path="/TM-Redux-task/cart" component={Cart} />
           </div>
         </div>
       </Router>
