922fa26e335473ce1f283f3ec4196be37c81a950
Gyyk <nazik.osmak@yahoo.com>
2019-08-01 20:47:07 +0300

    Updates

---
+++ precache-manifest.1ee51a1fd835676410d6b760bcbaed3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5cb79a6f30e05b694bdc83c9178d57b",
+    "url": "/TM-Redux-task/index.html"
+  },
+  {
+    "revision": "719de4343faafddd65be",
+    "url": "/TM-Redux-task/static/css/main.d4df5207.chunk.css"
+  },
+  {
+    "revision": "1affbdd3d2c87dc021b6",
+    "url": "/TM-Redux-task/static/js/2.7d97e6c3.chunk.js"
+  },
+  {
+    "revision": "719de4343faafddd65be",
+    "url": "/TM-Redux-task/static/js/main.3653f854.chunk.js"
+  },
+  {
+    "revision": "57d7947bc092d6e64729",
+    "url": "/TM-Redux-task/static/js/runtime~main.49722137.js"
+  }
+]);
